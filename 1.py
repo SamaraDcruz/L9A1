99fe6556002362bc7dec619ab0988ba9cb2e334e
SamaraDcruz <samaragracedcruz@gmail.com>
2025-06-04 02:55:11 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+# Example 1: Simple math
+result1 = 2 + 3 * 4 
+print("Result of 2 + 3 * 4 is:", result1)
+
+
+# Example 2: Using brackets
+result2 = (2+3) * 4
+print("Result of (2+3) * 4 is:", result2)
+
+# Example 3: Mix of +, -, *, and /
+result3 = 10 + 2 * 5 - 3
+print("Result of 10 + 2 * 5 - 3 is:", result3)
+
+
+# Example 4: With division
+result4 = 20 + 10 / 2
+print("Result of 20 + 10 / 2 is:", result4)
